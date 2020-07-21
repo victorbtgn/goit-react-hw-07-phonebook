fb277aa65b0d9d105a671a44a0558b0f0acbb0db
Victor Batigin <bixs85@gmail.com>
2020-07-21 17:45:47 +0300

    remove local storage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import store from './redux/store';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import './index.css';
@@ -10,10 +9,8 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store.store}>
-      <PersistGate loading={null} persistor={store.persistor}>
-        <App />
-      </PersistGate>
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
