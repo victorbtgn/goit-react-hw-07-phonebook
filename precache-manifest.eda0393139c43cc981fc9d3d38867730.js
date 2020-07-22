094db6f9da957e14af9a348d2d32dcf31130bf85
Victor Batigin <bixs85@gmail.com>
2020-07-22 18:34:00 +0300

    Updates

---
+++ precache-manifest.eda0393139c43cc981fc9d3d38867730.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab6876aae5f5cfb7864c339cd684868",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "9878c317803a722273b2",
+    "url": "/goit-react-hw-07-phonebook/static/css/2.368dc012.chunk.css"
+  },
+  {
+    "revision": "6b36ef0eae81b4c537bb",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.599cf583.chunk.css"
+  },
+  {
+    "revision": "9878c317803a722273b2",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.ac4ec873.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.ac4ec873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b36ef0eae81b4c537bb",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.a213aa9f.chunk.js"
+  },
+  {
+    "revision": "084b6989eb61407bc395",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.acd880ee.js"
+  }
+]);
